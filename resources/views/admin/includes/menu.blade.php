9df41f0cebfd9932e672a5ffd6465da985f84383
unknown <lucascelestiano@gmail.com>
2020-07-11 14:11:17 -0400

    11/07

--- resources/views/admin/includes/menu.blade.php
+++ resources/views/admin/includes/menu.blade.php
@@ -14,6 +14,42 @@
                               <span class="nav-link-text">Início</span>
                           </a>
                       </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="{{ route('admin.group.adicionar') }}">
+                              <i class="fas fa-book text-default"></i>
+                              <span class="nav-link-text">Grupos</span>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="{{ route('admin.category.adicionar') }}">
+                              <i class="fas fa-list-alt text-default"></i>
+                              <span class="nav-link-text">Categoria</span>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="{{ route('admin.brand.adicionar') }}">
+                              <i class="fas fa-copyright text-default"></i>
+                              <span class="nav-link-text">Marca</span>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="{{ route('admin.bull.adicionar') }}">
+                              <i class="far fa-file-alt text-default"></i>
+                              <span class="nav-link-text">Bulas</span>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="{{ route('admin.product.adicionar') }}">
+                              <i class="fas fa-syringe	"></i>
+                              <span class="nav-link-text">Produtos</span>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="{{ route('admin.salespeople.adicionar') }}">
+                              <i class="fas fa-users text-default"></i>
+                              <span class="nav-link-text">Vendedores</span>
+                          </a>
+                      </li>
                   </ul>
               </div>
           </div>
--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,56 @@ Route::get('/', 'Controller@index');
 Route::get('/contato', 'Controller@contact');
 Route::get('/produtos', 'Controller@products');
 Route::get('/ver', 'Controller@single');
-Route::get('/admin', 'Controller@admin');
 
+Route::get('/admin', 'Controller@admin');
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    
+    //Grupos
+    Route::group(['prefix' => 'group', 'as' => 'group.'], function(){
+        Route::get('adicionar', 'GroupController@index')->name('adicionar');
+        Route::post('add', 'GroupController@insert')->name('add');
+        Route::get('editar', 'GroupController@update')->name('editar');
+    });
+
+    //Categorias
+    Route::group(['prefix' => 'category', 'as' => 'category.'], function(){
+        Route::get('adicionar', 'CategoryController@index')->name('adicionar');
+        Route::post('add', 'CategoryController@insert')->name('add');
+        Route::get('editar', 'CategoryController@update')->name('editar');
+    });
+
+    //Marcas
+    Route::group(['prefix' => 'brand', 'as' => 'brand.'], function(){
+        Route::get('adicionar', 'BrandController@index')->name('adicionar');
+        Route::post('add', 'BrandController@insert')->name('add');
+        Route::get('editar', 'BrandController@update')->name('editar');
+    });
+    
+    //Vendedores
+    Route::group(['prefix' => 'salespeople', 'as' => 'salespeople.'], function(){
+        Route::get('adicionar', 'SalespeopleController@index')->name('adicionar');
+        Route::post('add', 'SalespeopleController@insert')->name('add');
+        Route::get('editar', 'SalespeopleController@update')->name('editar');
+    });
+    
+    //Bulas
+    Route::group(['prefix' => 'bull', 'as' => 'bull.'], function(){
+        Route::get('adicionar', 'BullController@index')->name('adicionar');
+        Route::post('add', 'BullController@insert')->name('add');
+        Route::get('editar', 'BullController@update')->name('editar');
+    });
+    
+    //Produtos
+    Route::group(['prefix' => 'product', 'as' => 'product.'], function(){
+        Route::get('adicionar', 'ProductController@index')->name('adicionar');
+        Route::post('add', 'ProductController@insert')->name('add');
+        Route::get('editar', 'ProductController@update')->name('editar');
+    });
+    
+    //Usuários
+    Route::group(['prefix' => 'user', 'as' => 'user.'], function(){
+        Route::get('adicionar', 'UserController@index')->name('adicionar');
+        Route::post('add', 'UserController@insert')->name('add');
+        Route::get('editar', 'UserController@update')->name('editar');
+    });
+});
