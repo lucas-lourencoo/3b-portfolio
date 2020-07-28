c583ba859c74688ad76106da30b774dfdb316454
lucascelestiano <lucascelestiano@gmail.com>
2020-07-28 10:48:12 -0400

    28/07

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -57,8 +57,9 @@
                     <h3 class="text-center">Localização</h3>
                     <div> 
                         <iframe style="border:0; width: 100%; height: 350px;"
-                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+                            src="https://maps.google.com/maps?q=Campo%20Grande%20-%20MS%20Rua%20treze%20de%20maio%2C%201191&t=&z=13&ie=UTF8&iwloc=&output=embed"
                             frameborder="0" allowfullscreen></iframe> 
+
                     </div>
                 </div>
             </div>
